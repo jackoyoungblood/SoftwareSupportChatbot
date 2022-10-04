061cd8a88fe2ffe90c1919b2f5fc20e1d9c58f14
Jack Youngblood <jackyoungblood@hotmail.com>
2022-10-04 15:27:47 -0400

    Add files via upload

--- gettoc.py
+++ gettoc.py
@@ -1,7 +1,11 @@
 import sys, fitz, pickle, os
+import openai 
+import wandb
 
-if not os.path.exists('C:\dev\pdfconversion\IRMWEB_toc.pickle'):
-    fname = 'C:\dev\pdfconversion\IRM Web Client User Guide (Legal Version) 10.3.3.pdf'
+keyword = 'relating'
+fname = 'C:\dev\pdfconversion\IRM Web Client User Guide (Legal Version) 10.3.3.pdf'
+
+if not os.path.exists('C:\dev\pdfconversion\IRMWEB_toc.pickle'):    
     doc = fitz.open(fname)  # open document
     out = open(fname + ".txt", "wb")  # open text output
     toclist = doc.get_toc(True)
@@ -15,7 +19,7 @@ llist = list()
 for i in range(len(toclist)):
     toclist[i] = str(toclist[i]).lower()
 
-filteredl = list(filter(lambda a: 'relating' in a, toclist))
+filteredl = list(filter(lambda a: keyword in a, toclist))
 
 pagel = []
 
@@ -25,5 +29,49 @@ for s in filteredl:
 
 
 pdfdoc = fitz.open(fname)  # open document
-pdfdoc.load_page(pagel[0]) #why isn't intellisense displaying load_page? I installed fitz and pymupf
+p = pdfdoc.load_page(pagel[0]) #why isn't intellisense displaying load_page? I installed fitz and pymupf
+text = p.get_text()
 
+outputtext = ''
+
+for line in text.splitlines():
+    if len(line) > 5:
+        outputtext += line
+        outputtext += '\n'
+
+text_file = open(f'c:\dev\pdfconversion\pagetext.txt', 'wt')
+n = text_file.write(outputtext)
+text_file.close()
+
+openai.api_key = os.getenv('OPENAIKEY')
+#run = wandb.init(project='SoftwareSupportChatbot')
+#prediction_table = wandb.Table(columns=["prompt", "completion"])
+
+gpt_prompt=''
+gpt_prompt += outputtext 
+gpt_prompt += '\n'
+gpt_prompt += 'Answer the question as truthfully as possible and if you are unsure of the answer say "Sorry I don''t know"'
+gpt_prompt += '\n'
+gpt_prompt += 'Q: How do I relate a filepart in iManage Records Manager?'
+#gpt_prompt += 'Q: How do I cancel a delivery request in imanage records manager?'
+gpt_prompt += '\n'
+gpt_prompt += 'A:'
+
+response = openai.Completion.create(
+  engine="text-davinci-002",
+  prompt=gpt_prompt,
+  temperature=0.0,
+  max_tokens=104,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
+)
+
+print(response['choices'][0]['text'])
+
+#prediction_table.add_data(gpt_prompt,response['choices'][0]['text'])
+#wandb.log({'predictions': prediction_table})
+#wandb.finish()
+
+test=''
+    
